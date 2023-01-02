c8f4f9ce7e01222e459215462b0050059eea1922
Mohd Hafizul Afifi Abdullah <16542981+hafizulamz@users.noreply.github.com>
2023-01-03 03:21:06 +0800

    Update setupenv.sh

--- baselines/DMBERT/setupenv.sh
+++ baselines/DMBERT/setupenv.sh
@@ -4,8 +4,3 @@ sudo apt-get update
 sudo apt install python3.7-dev
 sudo apt install python3.7-venv
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3.7
-
-#setup virtualenv in the current (DMBERT) folder
-python3.7 -m venv env
-source env/bin/activate
-pip install -r requirements.txt
