ab3191abe212504f64bf78fe07379807815947ec
Mohd Hafizul Afifi Abdullah <16542981+hafizulamz@users.noreply.github.com>
2023-01-18 01:50:49 +0800

    Create install-CUDA102.sh

---
+++ baselines/DMCNN_BiLSTM_(CRF)/install-CUDA102.sh
@@ -0,0 +1,19 @@
+#Get PPA repo drivers and Install CUDA 10.2
+#https://developer.nvidia.com/cuda-10.2-download-archive?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=debnetwork
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo rm cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+
+#Set the environment for cuda 10.2
+nano ~/.bashrc
+export PATH=/usr/local/cuda-10.2/bin${PATH:+:${PATH}}$ 
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+source ~/.bashrc
+
+## To verify the installation
+nvcc -V  # this should print the CUDA version installed on the system
